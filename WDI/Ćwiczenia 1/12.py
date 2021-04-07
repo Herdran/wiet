5f61ca5b73e34e31e44fc297cf482d20f198a269
Herdran <74078045+Herdran@users.noreply.github.com>
2021-04-08 00:14:58 +0200

    a
    aa

--- WDI/Ćwiczenia 1/12.py
+++
@@ -1,9 +0,0 @@
-x = 1023
-y = 576
-z = 209
-number = x
-while number > 0:
-    if x % number == 0 and y % number == 0 and z % number == 0:
-        print("A dzielnik najwienkszy", number)
-        number = 0
-    number -= 1
